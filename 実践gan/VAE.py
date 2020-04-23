a1fad007ad3f387c9a87a6463157c3769e64347b
mainichizenkyu <ryuchelswifty13@gmail.com>
2020-04-23 18:07:07 +0900

    最初のコミット

---
+++ 実践gan/VAE.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 18:03:39 2020
+
+@author: ryuch
+"""
+
+def hogehoge():
+    print("hogehoge")
+    
+    
+if __name__ == "__main__":
+    hogehoge()
